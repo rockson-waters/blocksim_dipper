15d7f8a89ed4c22d910d640a6029f456f155ef3a
rockson <71571551+rockson-waters@users.noreply.github.com>
2022-09-29 17:20:34 +0000

    retrieve node properties from json

--- blocksim/utils.py
+++ blocksim/utils.py
@@ -1,8 +1,13 @@
 import binascii
 from datetime import datetime
+import json
 import random
 from ast import literal_eval as make_tuple
+from typing import List
+import numpy as np
 import scipy.stats
+
+from blocksim.models.node import Node
 try:
     from Crypto.Hash import keccak
 
@@ -91,7 +96,8 @@ def get_random_values(distribution: dict, n=1):
     Distribution format: { \'name\': str, \'parameters\': tuple }"""
     dist = getattr(scipy.stats, distribution['name'])
     param = make_tuple(distribution['parameters'])
-    return dist.rvs(*param[:-2], loc=param[-2], scale=param[-1], size=n)
+    c = dist.rvs(*param[:-2], loc=param[-2], scale=param[-1], size=n)
+    return c
 
 
 def decode_hex(s):
@@ -116,3 +122,66 @@ def is_numeric(x):
 
 def encode_int32(v):
     return v.to_bytes(32, byteorder='big')
+
+
+
+rng = np.random.default_rng()
+sim_data = {}
+all_nodes = {}
+
+def _read_json_file(file_location:str):
+        with open(file_location) as f:
+            return json.load(f)
+
+
+def initialize_node_values(folder_path:str="out"):
+    node_properties = dict(_read_json_file(f"{folder_path}/node_properties.json"))
+    loc_names = list(_read_json_file(f"{folder_path}/loc_names.json"))
+    latencies = dict(_read_json_file(f"{folder_path}/latencies.json"))
+    throughputs = dict(_read_json_file(f"{folder_path}/throughput.json"))
+    solutions = dict(_read_json_file(f"{folder_path}/solutions.json"))
+
+    sim_data["number_of_nodes"] = num_nodes
+    sim_data["dim"] = get_average_number_of_neighbours(sim_data["number_of_nodes"])
+    sim_data["lb"] = [0] * sim_data["dim"] # Lower bound
+    sim_data["ub"] = [sim_data["number_of_nodes"] - 1] * sim_data["dim"] # Upper bound
+    sim_data["c_max"] = get_max_compute_capacity(all_peers) # maximum compute capacity in the network
+    sim_data["current_node_id"] = 0
+    pass
+
+def get_node_by_id(node_id:int):
+    node = all_nodes.get(node_id)
+    return node
+
+def get_nodes(node_ids:list):
+    nodes = []
+    for node_id in node_ids:
+        node = get_node_by_id(node_id)
+        nodes.append(node)
+    return nodes
+
+def get_optimum_neighbours(current_node_id:int):
+    pass
+
+def get_random_neighbours(num:int, current_node_id:int):
+    sim_data["current_node_id"] = current_node_id
+    a = np.random.randint(0, sim_data["number_of_nodes"], sim_data["dim"])
+    a = _check_and_fix_solution(a)
+    neighbours = []
+
+    return neighbours
+
+def _check_and_fix_solution(solution:np.ndarray):
+    
+    solution = solution.astype(int)
+    solution = solution.clip(sim_data["lb"], sim_data["ub"]) 
+    b = set(solution)
+
+    if b.__contains__(sim_data["current_node_id"]):
+        b.remove(sim_data["current_node_id"])
+
+    while ((len(solution) - len(b)) > 0):
+        b.add(np.random.randint(0, sim_data["number_of_nodes"], 1)[0])
+        if b.__contains__(sim_data["current_node_id"]):
+            b.remove(sim_data["current_node_id"])
+    return np.array(list(b))
